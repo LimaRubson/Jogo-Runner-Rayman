cafbf276fa80ee60c1abf6529fe032234ce55dff
LimaRubson <limarubson@hotmail.com>
2021-05-05 23:34:47 -0300

    first commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float Velocidade = 2;
+    public bool Pulo;
+    Rigidbody2D meuCorpo;
+    Animator meuAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meuCorpo = GetComponent<Rigidbody2D>();
+        meuAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float velX = Input.GetAxisRaw("Horizontal");		
+        meuCorpo.velocity = new Vector2(velX * Velocidade, meuCorpo.velocity.y);
+        if(Input.GetKeyDown("space"))
+		{
+			if(Pulo == false)
+			{
+				meuCorpo.velocity += new Vector2(0, 5);
+				Pulo = true;
+			}
+		}
+
+        if(velX	== 1f)
+          {
+            transform.rotation = new Quaternion(0,0,0,0);
+          }
+        else if(velX == -1f)
+            {
+                transform.rotation = new Quaternion(0, 180, 0, 0);
+            }
+        meuAnim.SetInteger("velocidade", Mathf.RoundToInt(meuCorpo.velocity.x));
+    }
+
+    void OnCollisionEnter2D(Collision2D outroObjeto)
+	{
+		Pulo = false;
+	}
+}
---
+++ Assets/Scripts/PontosControlador.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PontosControlador : MonoBehaviour
+{
+    public Text textoPontos;
+    public int pontos = 0;
+
+    // Colisões por Trigger -  Ganhar pontos
+    void OnTriggerEnter2D(Collider2D objeto)
+    {
+
+        if (objeto.tag == "Pontos")
+        {
+            pontos++;
+            textoPontos.text = pontos.ToString();
+
+            Destroy(objeto.gameObject);
+        }
+
+    }
+}
